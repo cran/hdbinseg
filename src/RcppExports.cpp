eb4ce286f618a9798546b6000bb42ba9af291898
Haeran Cho <haeran.cho@bristol.ac.uk>
2023-08-17 13:02:33 +0000

    version 1.0.2

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,6 +6,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // stl_sort
 NumericVector stl_sort(NumericVector x);
 RcppExport SEXP _hdbinseg_stl_sort(SEXP xSEXP) {
